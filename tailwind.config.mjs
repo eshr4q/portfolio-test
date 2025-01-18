47bf1df206fb043467078f7aa069f119ca653e38
Eshr4q <feshr4q@gmail.com>
2025-01-18 17:13:08 +0330

    debug tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,10 +5,40 @@ export default {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+	
   ],
+  prefix: "",
   theme: {
+	container: {
+		center: true,
+		padding: "15px",
+	},
+	screens: {
+		sm: "640px",
+		md: "768px",
+		lg: "960px",
+		xl: "1200px",
+	  },
+	fontFamily: {
+		primary: "var(--font-jetbrainsMono)",
+	  },
   	extend: {
+		keyframes: {
+			"accordion-down": {
+			  from: { height: "0" },
+			  to: { height: "var(--radix-accordion-content-height)" },
+			},
+			"accordion-up": {
+			  from: { height: "var(--radix-accordion-content-height)" },
+			  to: { height: "0" },
+			},
+		  },
   		colors: {
+			primary: '#1c1c22',
+			accent: {
+				DEFAULT: '#00ff99',
+				hover: '00e187',
+			},
   			background: 'var(--background)',
   			foreground: 'var(--foreground)'
   		},
