d103da55e950765fdb8a08abdf8606586854bce6
eshraq <feshr4q@gmail.com>
2025-02-03 12:50:13 +0330

    start with services section

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,8 +1,46 @@
+"use client";
+
+import { BsArrowDownRight } from "react-icons/bs";
+import Link from "next/link";
+
+import  { motion } from "framer-motion";
+
+const services = [
+  {
+    num: "81",
+    title: "Web Development",
+    description: "Lorem ipsum dolor sit, amet cnam nulla excepturi!",
+    href: ""
+  },
+  {
+    num: "82",
+    title: "UI/UX Design",
+    description: "Lorem ipsum dolor sit, amet cnam nulla excepturi!",
+    href: ""
+  },
+  {
+    num: "83",
+    title: "Logo Design",
+    description: "Lorem ipsum dolor sit, amet cnam nulla excepturi!",
+    href: ""
+  },
+  {
+    num: "84",
+    title: "SEO",
+    description: "Lorem ipsum dolor sit, amet cnam nulla excepturi!",
+    href: ""
+  },
+]
+
 
 
 const Services = () => {
   return (
-    <div>service page</div>
+    <section className="min-h-[80hv] flex-col justify-center py-12 xl:py-8">
+      <div className="container mx-auto">
+        service page
+      </div>
+    </section>
   )
 }
 
--- components/Stats.jsx
+++ components/Stats.jsx
@@ -24,7 +24,7 @@ const stats = [
 
 const Stats = () => {
   return (
-    <section>
+    <section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
         <div className="container mx-auto">
             <div className="flex flex-wrap gap-6 max-w-[83vw] ms-auto xl:max-w-none">
                 {stats.map((item, index) => {
@@ -34,13 +34,13 @@ const Stats = () => {
                         end={item.num} 
                         duration={5}  
                         delay={2}
-                        className="text-4xl xl:text-6xl font-extrabold"/>
-                        <p>
-
-
-                           
-                            
+                        className="text-4xl xl:text-6xl font-extrabold"
+                        />
+                        <p className={`${item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"} leading-snug text-white/80`} >
+                            {item.text}
                         </p>
+
+                        
                     </div>
                     )
                 })}
